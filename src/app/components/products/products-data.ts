33b9b9676fc9967533f3c833a383d74244636bb5
Julian Gamez <gamez.julian@gmail.com>
2018-02-06 13:12:40 -0300

    bla

--- src/app/components/products/products-data.ts
+++ src/app/components/products/products-data.ts
@@ -9,10 +9,34 @@ export class ProductData implements InMemoryDbService {
             {
                 'id': 1,
                 'code': '0000-AB',
-                'description': 'Product#1',
-                'name': 'Name#1',
+                'description': 'Ultrabook. SSD. RAM',
+                'name': 'HP Envy 13',
                 'price': 60,
-                'tags': ['tag#1', 'tag#2', 'tag#3']
+                'tags': ['HP', 'IT', 'Ultrabook']
+            },
+            {
+                'id': 2,
+                'code': '0000-AC',
+                'description': 'Monitor Samsung',
+                'name': 'Samsung LED 19',
+                'price': 20,
+                'tags': ['Samsung', 'Monitor', 'GAMER']
+            },
+            {
+                'id': 3,
+                'code': '0000-AD',
+                'description': 'Game card to run all the games with full resolution',
+                'name': 'ATI RADEON 999999',
+                'price': 90,
+                'tags': ['ATI', 'GAMER', 'NERD']
+            },            
+            {
+                'id': 4,
+                'code': '0000-AE',
+                'description': 'new IPAD',
+                'name': 'IPAD Retina 5',
+                'price': 80,
+                'tags': ['Apple', 'Macri Cat', 'Expensive', 'Cristi come back']
             }
         ];
         return { products };
